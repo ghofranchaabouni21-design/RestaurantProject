b7629bf211684c9401d45b5704477b68331f2e98
ghofran <ghofranchaabouni21@gmail.com>
2026-04-22 13:27:54 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import model.Plat;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Plat p = new Plat(1, "Pizza", 15.0);
+        System.out.println(p.getNom());
+
+    }
+}
